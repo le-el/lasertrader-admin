b52d851327d3f0a2eb55a02ea43e5e9d7b7029b2
le-el <a.corradini0215@gmail.com>
2025-02-13 02:16:42 +0900

    Add: formula setting

--- src/components/Formula.js
+++ src/components/Formula.js
@@ -44,7 +44,7 @@ const Formula = ({ openSidebar }) => {
     const [selectedFormula, setSelectedFormula] = useState(null);
     const [newFormula, setNewFormula] = useState({
         name: '',
-        pip_size: '',
+        formula: '',
     });
     const [errors, setErrors] = useState({});
     const [snackbarOpen, setSnackbarOpen] = useState(false);
@@ -87,7 +87,7 @@ const Formula = ({ openSidebar }) => {
     const validate = () => {
         const tempErrors = {};
         if (!newFormula.name) tempErrors.name = 'Name is required';
-        if (!newFormula.pip_size) tempErrors.pip_size = 'Pip size is required';
+        if (!newFormula.formula) tempErrors.formula = 'Formula is required';
         setErrors(tempErrors);
         return Object.keys(tempErrors).length === 0;
     };
@@ -108,7 +108,7 @@ const Formula = ({ openSidebar }) => {
                 });
                 fetchFormulas();
                 showSnackbar(res.data.message);
-                setNewFormula({ name: '', pip_size: '' });
+                setNewFormula({ name: '', formula: '' });
                 setErrors({});
                 setOpenCreateModal(false);
             } catch (error) {
@@ -183,7 +183,7 @@ const Formula = ({ openSidebar }) => {
                             <TableHead>
                                 <TableRow style={{ backgroundColor: 'rgb(13, 191, 150)', color: '#fff' }}>
                                     <TableCell style={{ color: '#fff', textAlign: 'center' }}>Name</TableCell>
-                                    <TableCell style={{ color: '#fff', textAlign: 'center' }}>PIP Size</TableCell>
+                                    <TableCell style={{ color: '#fff', textAlign: 'center' }}>Formula</TableCell>
                                     <TableCell style={{ color: '#fff', textAlign: 'center' }}>CreateAt</TableCell>
                                     <TableCell style={{ color: '#fff', textAlign: 'center' }}>Actions</TableCell>
                                 </TableRow>
@@ -192,7 +192,7 @@ const Formula = ({ openSidebar }) => {
                                 {formulas.map((formula) => (
                                     <TableRow key={formula.id}>
                                         <TableCell style={{ textAlign: 'center' }}>{formula.name}</TableCell>
-                                        <TableCell style={{ textAlign: 'center' }}>{formula.pip_size}</TableCell>
+                                        <TableCell style={{ textAlign: 'center' }}>{formula.formula}</TableCell>
                                         <TableCell style={{ textAlign: 'center' }}>{formatDate(formula.createdAt)}</TableCell>
                                         <TableCell style={{ textAlign: 'center' }}>
                                             <IconButton color="primary" onClick={() => handleEditFormula(formula)}>
@@ -212,9 +212,177 @@ const Formula = ({ openSidebar }) => {
                 )}
             </Box>
 
+            {/* Create Formula Modal */}
+            <Dialog
+                open={openCreateModal}
+                onClose={() => {
+                    setNewFormula({
+                        name: '',
+                        formula: '',
+                    });
+                    setErrors({});
+                    setOpenCreateModal(false);
+                }}
+            >
+                <DialogTitle>Create Formula</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Name"
+                        type="text"
+                        fullWidth
+                        variant="outlined"
+                        value={newFormula.name}
+                        onChange={(e) =>
+                            handleNewFormulaChange('name', e.target.value)
+                        }
+                        error={!!errors.name}
+                        helperText={errors.email}
+                        required
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Formula"
+                        type="text"
+                        fullWidth
+                        variant="outlined"
+                        value={newFormula.formula}
+                        onChange={(e) =>
+                            handleNewFormulaChange('formula', e.target.value)
+                        }
+                        error={!!errors.formula}
+                        helperText={errors.formula}
+                        required
+                    />
+
+                    {errors.formula && (
+                        <Typography color="error">
+                            {errors.formula}
+                        </Typography>
+                    )}
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        onClick={() => {
+                            setNewFormula({
+                                name: '',
+                                formula: '',
+                            });
+                            setErrors({});
+                            setOpenCreateModal(false);
+                        }}
+                        color="secondary"
+                    >
+                        Cancel
+                    </Button>
+                    <Button onClick={handleCreateFormula} color="primary">
+                        Create
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            {/* Edit Formula Modal */}
+            <Dialog
+                open={openEditModal}
+                onClose={() => setOpenEditModal(false)}
+            >
+                <DialogTitle>Edit Formula</DialogTitle>
+                <DialogContent>
+                    {selectedFormula && (
+                        <>
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                label="Name"
+                                type="text"
+                                fullWidth
+                                variant="outlined"
+                                value={selectedFormula.name}
+                                onChange={(e) =>
+                                    setSelectedFormula({
+                                        ...selectedFormula,
+                                        name: e.target.value,
+                                    })
+                                }
+                                error={!!errors.email}
+                                helperText={errors.email}
+                                required
+                            />
+                            <TextField
+                                label="Formula"
+                                type='text'
+                                margin='dense'
+                                fullWidth
+                                variant='outlined'
+                                value={selectedFormula.formula}
+                                onChange={(e) => {
+                                    setSelectedFormula({
+                                        ...selectedFormula,
+                                        formula: e.target.value,
+                                    });
+                                }}
+                                error={!!errors.email}
+                                helperText={errors.email}
+                                required
+                            />
+                        </>
+                    )}
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        onClick={() => setOpenEditModal(false)}
+                        color="secondary"
+                    >
+                        Cancel
+                    </Button>
+                    <Button onClick={handleUpdateFormula} color="primary">
+                        Update
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            {/* Delete Confirmation Modal */}
+            <Dialog
+                open={openDeleteModal}
+                onClose={() => setOpenDeleteModal(false)}
+            >
+                <DialogTitle>Confirm Deletion</DialogTitle>
+                <DialogContent>
+                    Are you sure you want to delete this Formula?
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        onClick={() => {
+                            setOpenDeleteModal(false);
+                            setNewFormula({
+                                name: '',
+                                formula: '',
+                            });
+                        }}
+                        color="secondary"
+                    >
+                        Cancel
+                    </Button>
+                    <Button onClick={handleDeleteFormula} color="primary">
+                        OK
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
             {/* Snackbar for notifications */}
-            <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose}>
-                <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{ width: '100%' }}>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={6000} // Duration to hide automatically
+                onClose={handleSnackbarClose}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            >
+                <Alert
+                    onClose={handleSnackbarClose}
+                    severity={snackbarSeverity}
+                    sx={{ width: '100%' }}
+                >
                     {snackbarMessage}
                 </Alert>
             </Snackbar>
